53e5b3f0323d9ca5670d1c661f63cc0cc8f9a802
VAegorov <kot2046@gmail.com>
2018-02-04 20:38:38 +0300

    Добавлена валидация логина и пароля

--- index.php
+++ index.php
@@ -49,44 +49,52 @@ if (isset($_POST['submit'])) {
         && !empty($_POST['language']) && !empty($_POST['email']) && !empty($_POST['login']) && !empty($_POST['password'])
         && !empty($_POST['password2'])) {
         //все поля заполнены
-        if ($_POST['password'] !== $_POST['password2']) {
-            echo "Пароли не совпадают.";
+        if ((strlen($_POST['password']) < 6 || strlen($_POST['password']) > 10) ||
+            (strlen($_POST['login']) < 4 || strlen($_POST['login']) > 12)) {
+            echo "Введите пароль длиной от 6 до 10 символов, логин - от 6 до 12.";
         } else {
-            //пароли совпадают, проверяем логин и email
-            $email = mysqli_real_escape_string($link, trim($_POST['email']));
-            $query = sprintf('SELECT email FROM authoriz WHERE email="%s"', $email);
-            $r = mysqli_query($link, $query);
-            if (mysqli_num_rows($r)) $mes_email = "email";
-
-            $login = mysqli_real_escape_string($link, trim($_POST['login']));
-            $query = sprintf('SELECT email FROM authoriz WHERE login="%s"', $login);
-            $r = mysqli_query($link, $query);
-            if (mysqli_num_rows($r)) $mes_log = "логин";
-
-            if (isset($mes_email)) echo "Указанный $mes_email занят, выберите другой.<br>";
-            if (isset($mes_log)) echo "Указанный $mes_log занят, выберите другой.<br>";
-
-            if (!isset($mes_email) && !isset($mes_log)) {
-                //логин и email свободны
-                $name = mysqli_real_escape_string($link, trim($_POST['name']));
-                $surname = mysqli_real_escape_string($link, trim($_POST['surname']));
-                $age = mysqli_real_escape_string($link, trim($_POST['age']));
-                $city = mysqli_real_escape_string($link, trim($_POST['city']));
-                $language = mysqli_real_escape_string($link, trim($_POST['language']));
-                $salt = generatorSalt();
-                $password = md5(trim($_POST['password']).$salt);
-                $date = date('Y-m-d H:i:s');
-                $query = sprintf('INSERT INTO authoriz (name, surname, age, city, language, password, email, login,
-                          date, salt) VALUES ("%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s")', $name, $surname,
-                            $age, $city, $language, $password, $email, $login, $date, $salt);
+            if ($_POST['password'] !== $_POST['password2']) {
+                echo "Пароли не совпадают.";
+            } else {
+                //пароли совпадают, проверяем логин и email
+                $email = mysqli_real_escape_string($link, trim($_POST['email']));
+                $query = sprintf('SELECT email FROM authoriz WHERE email="%s"', $email);
                 $r = mysqli_query($link, $query);
-                if (mysqli_affected_rows($link)) {
-                    //регистрация прошла успешно
-                    echo "Вы успешно зарегистрировались.";
-                    if (isset($pass)) echo "Ваш пароль: $pass";
+                if (mysqli_num_rows($r)) $mes_email = "email";
+
+                $login = mysqli_real_escape_string($link, trim($_POST['login']));
+                $query = sprintf('SELECT email FROM authoriz WHERE login="%s"', $login);
+                $r = mysqli_query($link, $query);
+                if (mysqli_num_rows($r)) $mes_log = "логин";
+
+                if (isset($mes_email)) echo "Указанный $mes_email занят, выберите другой.<br>";
+                if (isset($mes_log)) echo "Указанный $mes_log занят, выберите другой.<br>";
+
+                if (!isset($mes_email) && !isset($mes_log)) {
+                    //логин и email свободны
+                    $name = mysqli_real_escape_string($link, trim($_POST['name']));
+                    $surname = mysqli_real_escape_string($link, trim($_POST['surname']));
+                    $age = mysqli_real_escape_string($link, trim($_POST['age']));
+                    $city = mysqli_real_escape_string($link, trim($_POST['city']));
+                    $language = mysqli_real_escape_string($link, trim($_POST['language']));
+                    $salt = generatorSalt();
+                    $password = md5(trim($_POST['password']).$salt);
+                    $date = date('Y-m-d H:i:s');
+                    $query = sprintf('INSERT INTO authoriz (name, surname, age, city, language, password, email, login,
+                          date, salt) VALUES ("%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s")', $name, $surname,
+                        $age, $city, $language, $password, $email, $login, $date, $salt);
+                    $r = mysqli_query($link, $query);
+                    if (mysqli_affected_rows($link)) {
+                        //регистрация прошла успешно
+                        echo "Вы успешно зарегистрировались.";
+                        if (isset($pass)) echo "Ваш пароль: $pass";
+                    }
                 }
             }
         }
+
+
+
     } else {
         echo "Вы не заполнили все поля";
     }
